f4c8bd0550ccf752187d9a6c143d4becf2955494
Flavio <flavio.origgi@outlook.it>
2021-06-14 15:54:46 +0200

    add ciclo for-in e array con + oggetti

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
 
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 
-var studente ={
-    nome : 'flavio', 
-    cognome : 'origgi',
-    eta : '31'
-}
+var studente = {
+    nome: 'flavio', 
+    cognome: 'origgi',
+    eta: '31'
+};
+
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for(var k in studente){
+    console.log(studente[k]);    
+}
 
 // Creare un array di oggetti di studenti.
+
+var variStudenti = [
+    {
+        nome: 'flavio', 
+        cognome: 'origgi',
+        eta: '31'
+    },
+    {
+        nome: 'pinco', 
+        cognome: 'pallino',
+        eta: '19'
+    },
+    {
+        nome: 'pippo', 
+        cognome: 'baudo',
+        eta: '50'
+    }
+];
+
 
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 
